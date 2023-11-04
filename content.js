db67de68b293b405388bc120db71b6ca68b8ae72
chisler <alexander.chisler@gmail.com>
2023-11-04 16:37:09 +0000

    upd

--- content.js
+++ content.js
@@ -4,19 +4,18 @@ const useMockApi = false; // Set this to false to use the real API
 function extractAndSendArticleText() {
     const paragraphTags = document.querySelectorAll('p');
     const articleText = Array.from(paragraphTags).map(p => p.textContent.trim()).join('\n\n');
-    console.log("starting");
 
     if (articleText) {
         if (useMockApi) {
             // Mock sending the extracted article text to the API and receiving a response
             const mockApiResponse = [{
                 quote: "The home secretary claimed streets risked",
-                confidence: ['low', 'mid', 'high'][Math.floor(Math.random() * 3)], // Random confidence
-                reason_to_doubt: 'The quote may not be accurate.'
+                danger_level: ['low', 'mid', 'high'][Math.floor(Math.random() * 3)], // Random confidence
+                reason_for_doubt: 'The quote may not be accurate.'
             },];
 
             console.log('Success (Mock):', mockApiResponse);
-            highlightTextWithQuote(paragraphTags, mockApiResponse.quote, mockApiResponse.reason_to_doubt);
+            highlightTextWithQuote(paragraphTags, mockApiResponse.quote, mockApiResponse.reason_for_doubt);
         } else {
             sendArticleToApi(articleText);
         }
@@ -26,21 +25,38 @@ function extractAndSendArticleText() {
 }
 
 function sendArticleToApi(articleText) {
-    const apiUrl = `https://factcheck-api.vercel.app/factcheck?article=${encodeURIComponent(articleText.substring(0, 100))}`;
+    const apiUrl = `https://factcheck-api.vercel.app/factcheck`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
+    // Set up the options for the fetch request
+    const options = {
+        method: 'POST', // The API expects a POST request
+        headers: {
+            'Content-Type': 'text/plain' // The API expects plain text in the body
+        },
+        body: articleText.substring(0, 1500) // Send the article text directly as a string
+    };
+
+    // Send the request to the API
+    fetch(apiUrl, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data_array => {
             console.log('Success (API):', data_array);
-            Array.from(data_array).forEach(data => {
+            data_array.forEach(data => { // Assuming data_array is an array
                 console.log("processing", data);
-                highlightTextWithQuote(document.querySelectorAll('p'), data.quote, data.reason_to_doubt);
+                highlightTextWithQuote(document.querySelectorAll('p'), data.quote, data.reason_for_doubt, data.danger_level);
             });
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
+
+
 
 
 function createDoubtBubble(text, href) {
@@ -63,20 +79,48 @@ function createDoubtBubble(text, href) {
 function createBubble(text, href) {
     const bubble = document.createElement('span');
     bubble.className = 'bubble';
-    bubble.innerHTML = `<a href="${href}" target="_blank" style="text-decoration: none; color: #000;">${text}</a>`;
+    bubble.innerHTML = `<span>${text}<a href="${href}" target="_blank" style="text-decoration: none; color: white;">Source 🔗</a></span>`;
     return bubble;
 }
 
-function highlightTextWithQuote(paragraphTags, quote, reasonToDoubt, href) {
-    Array.from(paragraphTags).forEach(p => {
-        if (p.textContent.includes(quote)) {
-            p.style.color = 'red';
+function splitQuoteIntoChunks(quote, chunkSize = 4) {
+    const words = quote.split(' '); // Split the quote by whitespace
+    let chunks = [];
+
+    for (let i = 0; i < words.length; i += chunkSize) {
+        chunks.push(words.slice(i, i + chunkSize).join(' '));
+    }
+
+    return chunks;
+}
+
+function highlightTextWithQuote(paragraphTags, quote, reason_for_doubt, danger_level, href) {
+    const quoteChunks = splitQuoteIntoChunks(quote);
+    const paragraphsArray = Array.from(paragraphTags);
+
+    paragraphsArray.forEach((p, index) => {
+        // Check if any of the chunks are in this paragraph
+        const chunkPresent = quoteChunks.some(chunk => p.textContent.includes(chunk));
+        if (chunkPresent) {
+            // Highlight the paragraph
+            if (danger_level === "high") {
+                p.style.color = 'red';
+            } else if (danger_level === "mid") {
+                p.style.color = 'orange';
+            } else if (danger_level === "low") {
+                p.style.color = 'yellow';
+            }
             p.style.fontWeight = 'bold';
-            const doubtBubble = createDoubtBubble(reasonToDoubt, href); // Pass the URL as an argument
-            p.appendChild(doubtBubble);
+
+            // If this is the first paragraph that contains the quote, append the bubble
+            if (index === paragraphsArray.findIndex(p => p.textContent.includes(quoteChunks[0]))) {
+                const doubtBubble = createDoubtBubble(reason_for_doubt, href);
+                p.appendChild(doubtBubble);
+            }
         }
     });
 }
+
 
 
 // Listen for a message from the popup
